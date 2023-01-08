919be2c600c67c970763fb38a604b1a31b02793c
Ali Muhammad <ali9796408@gmail.com>
2023-01-09 02:22:54 +0500

    password reset page hash verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import { Inter } from '@next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
-    <main
-      className={`${inter.className} flex h-screen flex-col items-center justify-center`}
-    >
-      <h1 className="text-3xl font-bold">Next.js SaaS Boilerplate</h1>
-      <p>
-        Build Your Next SaaS Product with Fully-Featured Open-Source Next.js
-        Boilerplate
-      </p>
+    <main className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Home Page</h1>
     </main>
   );
 }
--- src/app/password-reset/[hash]/page.tsx
+++ src/app/password-reset/[hash]/page.tsx
@@ -1,4 +1,6 @@
-export default function PasswordReset() {
+export default function PasswordReset({ params }) {
+  // eslint-disable-next-line no-console
+  console.log(params);
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">Password Reset Page</h1>
